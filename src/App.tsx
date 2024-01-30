fdef478d104d58a555f4c2166fe6b5363d2ba6d2
Chattox <conradlineker@gmail.com>
2024-01-30 22:06:20 +0000

    feat: get all readings

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "./App.css";
+import { getAllReadings } from "./utils/api";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [readings, setReadings] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <MantineProvider>
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
-    </MantineProvider>
-  );
+  useEffect(() => {
+    getAllReadings().then((res) => {
+      setReadings(res);
+      setLoading(false);
+    });
+  }, []);
+
+  return <MantineProvider></MantineProvider>;
 }
 
 export default App;
