cc75a51f8840cfc857e3fe468de31262c72d0343
Chattox <conradlineker@gmail.com>
2024-01-31 13:47:29 +0000

    feat: shift api handling to individual component level

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { useEffect, useState } from "react";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
 import "./App.css";
-import { getAllReadings } from "./utils/api";
-import { formatReadings } from "./utils/formatReadings";
-import { FormattedReading } from "./types/global";
 import { ReadingsOverview } from "./components/ReadingsOverview";
 import { LatestReading } from "./components/LatestReading";
 
 function App() {
-  const [readings, setReadings] = useState<FormattedReading[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    getAllReadings().then((res) => {
-      setReadings(formatReadings(res));
-      setLoading(false);
-    });
-  }, []);
-
   return (
     <MantineProvider>
       <LatestReading />
-      {loading ? <p>loading</p> : <ReadingsOverview readings={readings} />}
+      <ReadingsOverview />
     </MantineProvider>
   );
 }
