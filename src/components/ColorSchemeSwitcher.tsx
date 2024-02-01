d35e0d17f66331b3e902577df1fcde6443ac01f2
Chattox <conradlineker@gmail.com>
2024-02-01 17:32:41 +0000

    fix: colour scheme switcher nsometimes not switching properly on first load

--- src/components/ColorSchemeSwitcher.tsx
+++ src/components/ColorSchemeSwitcher.tsx
@@ -1,16 +1,23 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import {
+  ActionIcon,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 
 export const ColorSchemeSwitcher = () => {
-  const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light");
 
   return (
     <ActionIcon
-      onClick={() => setColorScheme(colorScheme === "dark" ? "light" : "dark")}
+      onClick={() =>
+        setColorScheme(computedColorScheme === "dark" ? "light" : "dark")
+      }
       variant="default"
       size="xl"
     >
-      {colorScheme === "dark" ? (
+      {computedColorScheme === "dark" ? (
         <IconMoon stroke={1.5} />
       ) : (
         <IconSun stroke={1.5} />
