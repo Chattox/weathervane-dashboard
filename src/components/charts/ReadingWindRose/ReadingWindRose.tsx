50c4cf78193b35c9d0c1513f3b087d2f125cc4d8
Chattox <conradlineker@gmail.com>
2024-02-03 21:55:43 +0000

    feat: wip windrose attempt with chartjs this time

--- src/components/charts/ReadingWindRose/ReadingWindRose.tsx
+++ src/components/charts/ReadingWindRose/ReadingWindRose.tsx
@@ -1,29 +1,47 @@
+import { PolarArea } from "react-chartjs-2";
 import {
-  Chart as WindRoseChart,
-  calculateWindRose,
-} from "react-windrose-chart";
+  ArcElement,
+  Chart as ChartJS,
+  Legend,
+  RadialLinearScale,
+  Tooltip,
+} from "chart.js";
 import { WindData } from "../../../types/global";
 
 export const ReadingWindRose = (props: { data: WindData }) => {
-  console.log(props.data);
-  const windRoseData = calculateWindRose(props.data);
-  const columns = [
-    "angle",
-    "0-1",
-    "1-2",
-    "2-3",
-    "3-4",
-    "4-5",
-    "5-6",
-    "6-7",
-    "7+",
-  ];
+  ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+  const data = {
+    labels: ["Red", "Green", "Yellow", "Grey", "Blue", "f", "g", "h"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [11, 16, 7, 3, 14, 20, 12, 6],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(75, 192, 192)",
+          "rgb(255, 205, 86)",
+          "rgb(201, 203, 207)",
+          "rgb(54, 162, 235)",
+        ],
+      },
+    ],
+  };
+
   return (
-    <WindRoseChart
-      chartData={windRoseData}
-      columns={columns}
-      width={300}
-      height={300}
+    <PolarArea
+      data={data}
+      options={{
+        scales: {
+          r: {
+            startAngle: 22.5,
+          },
+        },
+        elements: {
+          arc: {
+            angle: 45,
+          },
+        },
+      }}
     />
   );
 };
