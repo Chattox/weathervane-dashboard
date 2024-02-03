288ed8753c9bc58ebee97d75e49b4676e8d496e7
Chattox <conradlineker@gmail.com>
2024-02-03 17:02:21 +0000

    feat: reading bar chart

--- src/components/ReadingsHistoryContainer/ReadingsHistoryContainer.tsx
+++ src/components/ReadingsHistoryContainer/ReadingsHistoryContainer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FormattedReading } from "../../types/global";
+import { FormattedReading, IndividualReadingData } from "../../types/global";
 import { getAllReadings } from "../../utils/api";
 import { formatReadings } from "../../utils/formatReadings";
 import { READINGS_LABELS } from "../../consts";
@@ -13,6 +13,7 @@ import {
 import classes from "./ReadingsHistoryContainer.module.css";
 import { ReadingAreaChart } from "../charts/ReadingAreaChart";
 import { getIndividualReadingHistory } from "../../utils/getIndividualReadingHistory";
+import { ReadingBarChart } from "../charts/ReadingBarChart";
 
 export const ReadingsHistoryContainer = () => {
   const [readings, setReadings] = useState<FormattedReading[]>([]);
@@ -27,6 +28,28 @@ export const ReadingsHistoryContainer = () => {
     });
   }, []);
 
+  const chartTypes: Record<string, string> = {
+    pressure: "area",
+    rain: "bar",
+    wind_speed: "area",
+    temperature: "area",
+    humidity: "area",
+    wind_direction: "area",
+    rain_per_second: "bar",
+    luminance: "area",
+  };
+
+  const getChart = (data: IndividualReadingData[], measurement: string) => {
+    switch (chartTypes[measurement]) {
+      case "area":
+        return <ReadingAreaChart data={data} measurement={measurement} />;
+      case "bar":
+        return <ReadingBarChart data={data} measurement={measurement} />;
+      default:
+        return undefined;
+    }
+  };
+
   const historyDisplays = Object.keys(READINGS_LABELS).map(
     (measurement: string) => {
       const data = getIndividualReadingHistory(readings, measurement);
@@ -37,7 +60,7 @@ export const ReadingsHistoryContainer = () => {
               <Text size="lg" fw={500} pl={16}>
                 {READINGS_LABELS[measurement].label}
               </Text>
-              <ReadingAreaChart data={data} measurement={measurement} />
+              {getChart(data, measurement)}
             </Stack>
           </Paper>
         </Grid.Col>
---
+++ src/components/charts/ReadingBarChart/index.ts
@@ -0,0 +1,1 @@
+export { ReadingBarChart } from "./ReadingBarChart";
